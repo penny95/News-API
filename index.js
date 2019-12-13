7402e953d57ffdbd7db6ee5405ab653db1874cb1
penny95 <49157799+penny95@users.noreply.github.com>
2019-12-13 22:17:11 +0300

    Improved syntax

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ process.stdin.once('data', (newsSource)=>{
 
     function newsApi () {
 
-    if(newsSource===1){
+    if(newsSource === 1){
       Request.get(`https://newsapi.org/v2/top-headlines?sources=the-new-york-times&pageSize=10&apiKey=fb616287f7c9405aa291c7e364dbde17`, (error,response,body)=>{
       if(error){
         console.log(error)
@@ -17,7 +17,7 @@ process.stdin.once('data', (newsSource)=>{
       console.log(JSON.parse(body));
     })
   }
-    else if(newsSource===2){
+    else if(newsSource === 2){
       Request.get(`https://newsapi.org/v2/top-headlines?sources=cnn&pageSize=10&apiKey=fb616287f7c9405aa291c7e364dbde17`, (error,response,body)=>{
         if(error){
           console.log(error)
@@ -25,7 +25,7 @@ process.stdin.once('data', (newsSource)=>{
       console.log(JSON.parse(body));
     })
   }
-    else if(newsSource===3){
+    else if(newsSource === 3){
       Request.get(`https://newsapi.org/v2/top-headlines?sources=usa-today&pageSize=10&apiKey=fb616287f7c9405aa291c7e364dbde17`, (error,response,body)=>{
         if(error){
           console.log(error)
@@ -33,7 +33,7 @@ process.stdin.once('data', (newsSource)=>{
       console.log(JSON.parse(body));
     })
   }
-    else if(newsSource===4){
+    else if(newsSource === 4){
       Request.get(`https://newsapi.org/v2/top-headlines?sources=nbc-news&pageSize=10&apiKey=fb616287f7c9405aa291c7e364dbde17`, (error,response,body)=>{
         if(error){
           console.log(error)
